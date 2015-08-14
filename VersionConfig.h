dff0b0f6571000620ac5612795798be02f9ff7ef
zcy <zhangchaoyang@aisino.com>
2015-08-14 08:45:27 +0800

    修改配置文件

--- VersionConfig.h
+++ VersionConfig.h
@@ -1,14 +1,26 @@
 #ifndef __LIB_VERSION_CONFIG_H
 #define __LIB_VERSION_CONFIG_H
 
-#define SOFTWARE_VERSION		1.000033		//软件版本号(对应debug版本号：1.110013)
+#define SOFTWARE_VERSION		1.000039		//软件版本号(对应debug版本号：1.110013)
 
 
 #define PROJECT_TYPE_ARM9		1		/**< ARM9转换器工程 */
 #define PROJECT_TYPE_ZJJ		2		/**< 中间件工程 */
-#define PROJECT_TYPE_A5			3		/**< A5转换器工程 */
+#define PROJECT_TYPE_A5_ZHH		3		/**< A5转换器工程 */
+#define PROJECT_TYPE_A5_YTJ		4		/**< A5一体机工程 */
 
-#define PROJECT_TYPE_MODE		PROJECT_TYPE_A5
+#define PROJECT_TYPE_MODE		PROJECT_TYPE_A5_ZHH
+
+
+#define NET_VERSION					1		//网络版本
+#define NO_NET_VERSION				2		//无网络版本
+#define BLUETOOTH_VERSION			3		//蓝牙版本
+
+#if(PROJECT_TYPE_MODE == PROJECT_TYPE_A5_YTJ)
+#define COMMUNICATE_VERSION			NO_NET_VERSION
+#else
+#define COMMUNICATE_VERSION			NET_VERSION
+#endif
 
 
 #define BUSINESS_JSK_MODE	1		//转换器方式调用金税盘流程接口
